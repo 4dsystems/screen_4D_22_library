3d1ebc5038676d482e953fee08ddacd5557d498f
Johan Cronje <jcronje@hotmail.com>
2017-10-31 14:36:24 -0700

    Aesthetical define change

--- src/DefaultFonts.c
+++ src/DefaultFonts.c
@@ -1,7 +1,7 @@
-#if defined (ESP8266)
-#include <pgmspace.h>
-#else
-#include <avr/pgmspace.h>
+#ifdef __AVR__
+  #include <avr/pgmspace.h>
+#elif defined(ESP8266) || defined(ESP32)
+  #include <pgmspace.h>
 #endif
 
 #define fontdatatype const uint8_t
